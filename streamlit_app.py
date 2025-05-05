8e54ce027753a8af070f9eb8fa2389dee46e339f
Thomasc2495 <thomasc.611797@gmail.com>
2025-05-05 02:16:10 +0000

    Email Project

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,36 @@
 import streamlit as st
+from openai import OpenAI
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+"""
+# Hello World, Streamlit!
+
+This is a website to demonstrate Streamlit's API.
+You can stop looking at this now.
+
+Please.
+"""
+
+
+with st.form("My form"):
+    name = st.text_input("What is your name? ")
+    recipient = st.text_input("Who are you sending this to?")
+    style = st.text_input("Input a writing style: ")
+    email = st.text_area("What do you want the email to be about? ")
+
+
+
+    system_prompt = f"You are an email writer writing from a person named {name} to a person named {recipient}."
+    user_prompt = f"Make the email about {email} in a {style} tone"
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+    )
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write(response.choices[0].message.content)
